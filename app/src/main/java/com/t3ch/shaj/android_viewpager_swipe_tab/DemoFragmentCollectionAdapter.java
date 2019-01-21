49241a343bf4837fa674682eaf05cc03e51810af
SHAJ <shakilahmedshaj@gmail.com>
2019-01-22 00:21:41 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/android_viewpager_swipe_tab/DemoFragmentCollectionAdapter.java
@@ -0,0 +1,8 @@
+package com.t3ch.shaj.android_viewpager_swipe_tab;
+
+/**
+ * Created by Shakil Ahmed Shaj on 22-Jan-19.
+ * shakilahmedshaj@gmail.com
+ */
+public class DemoFragmentCollectionAdapter {
+}
